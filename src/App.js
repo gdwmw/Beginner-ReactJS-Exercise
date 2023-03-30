c5ebeacba1aeb394f5f466c96d8838b78d6640d6
Landensaki <gededewowahyumw@gmail.com>
2023-03-30 10:50:09 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MenuMakanan from "./components/MenuMakanan/MenuMakanan";
+import MenuMinuman from "./components/MenuMinuman/MenuMinuman";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      namaResto: "Landensaki",
+      menuMakanan: [
+        { nama: "Ayam Tulang Lunak", harga: 28000, stock: 8 },
+        { nama: "Ayam Rica - Rica", harga: 25000, stock: 11 },
+      ],
+    };
+  }
+
+  handleGantiNama = (nama) => {
+    this.setState({ namaResto: nama });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Cafe and Resto {this.state.namaResto}</h1>
+        <button
+          className="btn btn-primary"
+          onClick={() => this.handleGantiNama("HobbiTxy")}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Ganti Nama Resto
+        </button>
+        <h2>Menu Makanan</h2>
+        <MenuMakanan namaMenu={"Ayam Bakar"} hargaMenu={25000} stockMenu={10} />
+        <MenuMakanan namaMenu={"Ayam Goreng"} hargaMenu={20000} stockMenu={0} />
+        <MenuMakanan
+          namaMenu={this.state.menuMakanan[0].nama}
+          hargaMenu={this.state.menuMakanan[0].harga}
+          stockMenu={this.state.menuMakanan[0].stock}
+        />
+        <MenuMakanan
+          namaMenu={this.state.menuMakanan[1].nama}
+          hargaMenu={this.state.menuMakanan[1].harga}
+          stockMenu={this.state.menuMakanan[1].stock}
+        />
+        <h2>Menu Minuman</h2>
+        <MenuMinuman namaMenu={"Juice Melon"} hargaMenu={15000} />
+        <MenuMinuman namaMenu={"Juice Apple"} hargaMenu={12000} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/MenuMakanan/MenuMakanan.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+//FUNCTION BASE
+var menuMakanan = (props) => {
+  if (props.stockMenu !== 0) {
+    return (
+      <div style={{ border: "1px solid black", width: 300, margin: "auto" }}>
+        <p>Nama Menu : {props.namaMenu}</p>
+        <p>Harga : Rp. {props.hargaMenu}</p>
+        <p>Stock : {props.stockMenu}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default menuMakanan;
